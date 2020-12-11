624beba5e4ade0dd389a6a305f48572423fdbe87
sibesh1 <sibesh1998@gmail.com>
2020-12-11 19:46:53 +0530

    refactored code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const Contact = require("./models/mongo");
 
 //MIDDLEWARE
