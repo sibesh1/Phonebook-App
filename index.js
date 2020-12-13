529f6c631f3e6db0d101f3036683a08b570b02cf
sibesh1 <sibesh1998@gmail.com>
2020-12-13 15:21:24 +0530

    removed popup alert

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
   if (error.name === "CastError") {
+    alert("Name or Number is missing");
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
